8289c3b17c84348946b95c78fb2cfd7fcbc5a806
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2021-11-13 12:34:58 +0600

    Сделана верстка компонентов menu-items и order-info

---
+++ src/app/Shared/menu-item.modal.ts
@@ -0,0 +1,4 @@
+export class Item {
+  constructor(public icon: string, public name: string, public price: number) {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuItemsComponent } from './menu-items/menu-items.component';
+import { OrderInfoComponent } from './order-info/order-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuItemsComponent,
+    OrderInfoComponent
   ],
   imports: [
     BrowserModule
